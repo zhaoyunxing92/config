f1e18024243344add7d0662a145f21bc48e66e65
zhaoyunxing <zhaoyunxing@apache.org>
2022-08-11 01:28:52 +0800

    init

---
+++ pkg/config/config.go
@@ -0,0 +1,10 @@
+package config
+
+type Config struct {
+	Name string
+	Path string
+}
+
+func Load() {
+
+}
---
+++ pkg/extension/config.go
@@ -0,0 +1,48 @@
+package extension
+
+import (
+	"log"
+	"sort"
+	"sync"
+)
+
+var (
+	configs map[string]Config
+	lock    sync.Mutex
+)
+
+type Config interface {
+	Prefix() string
+
+	Load() error
+
+	Order() int
+}
+
+func Register(name string, config Config) {
+	lock.Lock()
+	defer lock.Unlock()
+	if configs != nil {
+		_, found := configs[name]
+		if found {
+			log.Fatalf("config plugin %s was registered twice", name)
+		}
+	} else {
+		configs = map[string]Config{}
+	}
+
+	configs[name] = config
+}
+
+func GetConfigs() []Config {
+	lock.Lock()
+	defer lock.Unlock()
+	var cs []Config
+	for _, config := range configs {
+		cs = append(cs, config)
+	}
+	sort.Slice(cs, func(i, j int) bool {
+		return cs[i].Order() < cs[j].Order()
+	})
+	return cs
+}
